ebac904d104fbd169f09447aff9325b32c00e53b
yutee <yuteenightxi@gmail.com>
2024-12-10 23:10:43 +0100

    update test

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -33,3 +33,23 @@ variable "hostnames" {
   type        = list(string)
   default     = ["@", "www", "traefik", "db"]
 }
+
+variable "tenant_id" {
+  description = "The Azure tenant ID."
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The Azure subscription ID."
+  type        = string
+}
+
+variable "client_id" {
+  description = "The Azure client ID."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Azure client secret."
+  type        = string
+}
