335fb78a88b47a4a446fb3325d1a35f38b0ed2f6
yutee <yuteenightxi@gmail.com>
2024-12-15 23:37:58 +0100

    add application code

---
+++ frontend/vite.config.ts
@@ -0,0 +1,12 @@
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), TanStackRouterVite()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+})
